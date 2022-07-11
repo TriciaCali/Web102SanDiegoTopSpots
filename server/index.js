4315ce708071bc22877f55244d6d5b06aeeb8def
Latricia Hunter <latricia@Latricias-iMac.lan>
2022-07-11 15:04:10 -0700

    edited server and index files for heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-/* eslint no-console: "off" */
 const server = require('./server');
-
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
